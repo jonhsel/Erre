931d4c7095f653c45e47f2069fa7304ef4488686
Jonh Selmo <jonhsel@gmail.com>
2021-07-12 17:18:57 -0300

    Funcoes Apply end

--- 05-apply.R
+++ 05-apply.R
@@ -30,12 +30,61 @@ sapply(lista1, sum)
 #calculando a media
 sapply(lista1, mean)
 
-
+#===========================
 #apply
 #criando a matrix
  x <- matrix(rnorm(9), nrow=3, byrow = T)
 x 
 
-apply(x, 1, mean) #calculando a media por linha
-apply(x, 2, mean) #calculando a media por coluna
+apply(x, 1, mean) #calculando a media por linha (number 1)
+apply(x, 2, mean) #calculando a media por coluna (numer 2)
 apply(x, 1, plot)
+
+#============================
+#Data Frame
+escola <- data.frame(Aluno =c('Alan', 'Alice', 'Alana', 'Aline'),
+                     Matematica = c(90, 80, 70, 60),
+                     Geografia = c(100, 85, 96, 78),
+                     Quimica = c(74, 85, 98, 77))
+
+escola
+#acessando a coluna
+escola$Geografia
+
+#Calculando a media por aluno
+#criar uma nova coluna com valores NA
+escola$Media = NA
+escola
+
+#atribuir os valores na coluna media
+escola$Media = apply(escola[, c(2,3,4)], 1, mean)
+#escola[, c(2,3,4)] = siginifca que pega todas as linhas
+# e as colunas 2, 3 e 4
+escola
+
+#Arredondando os valores da coluna media
+escola$Media = round(escola$Media)
+escola
+
+
+#======================
+#tapply vs sqldf
+install.packages('sqldf')
+require(sqldf)
+
+escola2 <- data.frame(Aluno = c('Alan', 'Alana', 'Aline', 'Alaor', 'Alan', 'Alana', 'Aline', 'Alaor'),
+                      Semestre = c(1, 1, 1, 1, 2, 2, 2, 2),
+                      matematica = c(90, 87, 88, 98, 78, 96, 99, 88),
+                      Geografia = c(100, 87, 98, 78, 96, 55, 88, 74),
+                      Quimica =   c(78, 65, 89, 45, 79, 99, 96, 97))
+escola2
+
+sqldf("select Aluno, sum(Matematica), sum(Geografia), sum(Quimica) from escola2 group by Aluno ")
+#utilizando o tapply
+tapply(c(escola2$matematica), escola2$Aluno, sum)
+
+
+##############
+#by
+sqldf("select aluno, sum(matematica) from escola2 group by semestre")
+by(escola2[, c(2,3,4)], escola2$Semestre, colSums)
